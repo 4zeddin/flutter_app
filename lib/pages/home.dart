a92363ce36cf086170cfbb449c4b887759d0428e
4zeddin <azeddinaitwaalla@gmail.com>
2024-02-08 23:39:24 +0100

    nav

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,26 +7,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Breakfast', 
-          style: TextStyle(color: Colors.black, fontSize:18, fontWeight: FontWeight.bold)
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0, //remove shadows
-        centerTitle: true,
-        leading: Container(
-          child: SvgPicture.asset('assets/icons/leftArrow.svg',
-          height: 28,
-          width: 28,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text('Breakfast',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold)),
+      backgroundColor: Colors.white,
+      elevation: 0.0, //remove shadows
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          // Action when leading icon is tapped
+        },
+        child: Container(
+          child: SvgPicture.asset(
+            'assets/icons/leftArrow.svg',
+            height: 28,
+            width: 28,
           ),
           margin: EdgeInsets.all(10),
           alignment: Alignment.center,
           decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(10)),
         ),
       ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+            // Action when action icon is tapped
+          },
+          child: Container(
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 28,
+              width: 28,
+            ),
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            decoration: BoxDecoration(
+                color: Color(0xffF7F8F8),
+                borderRadius: BorderRadius.circular(10)),
+          ),
+        )
+      ],
     );
   }
 }
